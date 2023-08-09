018967fc9b63a1a0dcb6dd4195ab92c3aed81b6c
alek <wait82985@gmail.com>
2023-08-09 12:24:58 +0600

    algo works but TLE 1542 / 1582

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,35 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     public int minimizeMax(int[] nums, int p) {
-        //TODO
-        return 0;
+        Arrays.sort(nums);
+        int[] absDiffs = new int[nums.length];
+        absDiffs[0] = Integer.MAX_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+            absDiffs[i] = Math.abs(nums[i] - nums[i - 1]);
+        }
+        Map<String, Integer> memo = new HashMap<>();
+
+        return dfs(absDiffs, p, 1, 0, memo);
     }
+
+    private int dfs(int[] absDiffs, int p, int i, int maxDif, Map<String, Integer> memo) {
+        if(memo.containsKey(p + " " + i + " " + maxDif)){
+            return memo.get(p + " " + i + " " + maxDif);
+        }
+        if (p == 0) {
+            return maxDif;
+        }
+        if (i >= absDiffs.length) {
+            return Integer.MAX_VALUE / 2;
+        }
+
+        int result = Math.min(dfs(absDiffs, p - 1, i + 2, Math.max(maxDif, absDiffs[i]), memo), dfs(absDiffs, p, i + 1, maxDif, memo));
+        memo.put(p + " " + i + " " + maxDif, result);
+        return result;
+    }
+
 }
